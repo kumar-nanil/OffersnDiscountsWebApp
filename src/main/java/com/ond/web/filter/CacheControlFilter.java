63e2816aa3ab71c0383ae013518c0d0c71866dc3
Anil <kumar.nanil@gmail.com>
2016-09-28 22:56:42 +0530

    Cache Control Filter added

--- src/main/java/com/ond/web/filter/CacheControlFilter.java
+++ src/main/java/com/ond/web/filter/CacheControlFilter.java
@@ -1,29 +1,23 @@
 package com.ond.web.filter;
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
-public class CacheControlFilter implements Filter {
+public class CacheControlFilter extends OncePerRequestFilter {
 
-    public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.setHeader("Expires", "Tue, 03 Jul 2001 06:00:00 GMT");
+        httpServletResponse.setDateHeader("Last-Modified", new Date().getTime());
+        httpServletResponse.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+        httpServletResponse.setHeader("Pragma", "no-cache");
 
-    }
-
-    public void doFilter(ServletRequest request, ServletResponse response,
-                         FilterChain chain) throws IOException, ServletException {
-
-        HttpServletResponse resp = (HttpServletResponse) response;
-        resp.setHeader("Expires", "Tue, 03 Jul 2001 06:00:00 GMT");
-        resp.setDateHeader("Last-Modified", new Date().getTime());
-        resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
-        resp.setHeader("Pragma", "no-cache");
-
-        chain.doFilter(request, response);
-    }
-
-    public void destroy() {
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
 
     }
 
