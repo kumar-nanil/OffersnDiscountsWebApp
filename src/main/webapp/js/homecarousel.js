efbb64c3c0b4f95fc45b6a92c3c42e418fbf9f5f
Anil <kumar.nanil@gmail.com>
2016-09-28 22:18:31 +0530

    HeadCarousel url change

--- src/main/webapp/js/homecarousel.js
+++ src/main/webapp/js/homecarousel.js
@@ -4,7 +4,7 @@
 //Home Carousel Controller
 app.controller('HomeCarouselCtrl', function($scope,$http) {
     self.carousels = [{}];
-    $http.get("http://localhost:8080/service/headCarousel").then(function(response) {
+    $http.get("https://offersndiscounts-142723.appspot.com/service/headcarousel").then(function(response) {
         $scope.carousels = response.data;
         console.log(response.mainCarousels);
     })
